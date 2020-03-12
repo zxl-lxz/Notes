408559b16c926c35f4d4a8ac1c63f0541b0b08d1
zela.zhou <1282772905@qq.com>
2020-03-12 16:29:16 +0800

    递归和Math.random

---
+++ JavaScript/递归函数和两个数之间的随机数/index.js
@@ -0,0 +1,55 @@
+// 两个数之间的随机数，包含最小，不包含最大
+const fun1 = (min, max) => {
+    return Math.random() * (max - min) + min;
+};
+
+// ------------------------------------------------------------
+
+// 两个数之间的随机整数，包含最小，不包含最大
+const fun2 = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+// --------------------------------------------------------------------
+
+// 两个数之间的随机整数，包含两个数
+const fun3 = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1 )) + min;
+};
+
+// -----------------------------------------------------------------------------
+
+// 用递归创造一个长度为5，值在2-32之间的整数，不重复。
+let i = 0;
+
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomArr(arr, num) {
+    if (arr.indexOf(num) < 0) {
+        arr[i] = num;
+        i++;
+    } else {
+        num = randomNum(2, 32);
+    }
+    if (i >= arr.length) {
+        console.log(arr);
+        return;
+    } else {
+        randomArr(arr, num);
+    }
+}
+let arr = new Array(5);
+let num = randomNum(2, 32);
+randomArr(arr, num);
+
+
+
+
+
+
