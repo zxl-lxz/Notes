8789450a968df9ec5fb385c988477e476c8d5056
zela.zhou <1282772905@qq.com>
2020-04-03 13:30:35 +0800

    各种数据类型的深拷贝（防止循环调用）

---
+++ JavaScript/深拷贝/index.js
@@ -0,0 +1,48 @@
+let deepClone = (obj, cache= []) => {
+    // null
+    // typeof 'a' === 'string'
+    // typeof 1 === 'number'
+    // typeof undefined === 'undefined'
+    // typeof function === 'function'
+    // typeof NaN === 'number'
+    // typeof Symbol() === 'symbol'
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+    // new String()
+    if (obj instanceof String) {
+        return new String(obj.valueOf());
+    }
+    // new Boolean()
+    if (obj instanceof Boolean) {
+        return new Boolean(obj.valueOf());
+    }
+    // new Number();
+    if (obj instanceof Number) {
+        return new Number(obj.valueOf());
+    }
+    // new Date()
+    if (obj instanceof Date) {
+        return new Date(obj.valueOf());
+    }
+    // /\d+/g  new RegExp(/\d+/g)
+    if (obj instanceof RegExp) {
+        return new RegExp(obj.valueOf());
+    }
+
+    // 防止循环调用：每次将obj存储，并判断是否已经存在，若存在，不再进行递归。
+    let hit = cache.filter((item) => item.origin === obj)[0];
+    if (hit) {
+        return hit.newObj;
+    }
+    const newObj = obj instanceof Array ? [] : {};
+    cache.push({
+        origin: obj,
+        newObj,
+    });
+    // typeof [] === 'object'
+    // typeof {} === 'object'
+    for (let k in obj) {
+        newObj[k] = deepClone(obj[k], cache);
+    }
+}
