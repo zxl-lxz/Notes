9b4d1706173329fb47a957a1bc619e37e238ff3f
zela.zhou <1282772905@qq.com>
2020-11-06 15:26:14 +0800

    手写call、apply

--- JavaScript/bind、call、apply/apply.js
+++ JavaScript/bind、call、apply/apply.js
@@ -1,7 +1,7 @@
 // apply
 // 指定this的值去执行函数。第二个参数是一个数组。
 
-let foo = function(x, y) {
+let foo = function (x, y) {
     this.x = x;
     this.y = y;
 }
@@ -11,3 +11,17 @@ let obj = {};
 foo.apply(obj, [1, 2]);
 
 console.log(obj); // {x: 1, y: 2}
+
+// 手写apply
+
+Function.prototype._apply = function (context) {
+    if (typeof this !== 'function') {
+        throw new TypeError('not function');
+    }
+    context = context || window;
+    context.fn = this;
+    let args = [...arguments][1];
+    let result = context.fn(...args);
+    delete context.fn;
+    return result;
+}
--- JavaScript/bind、call、apply/call.js
+++ JavaScript/bind、call、apply/call.js
@@ -1,8 +1,23 @@
 // call
+
 // 用指定的this值去执行函数。第一个参数后的参数以逗号分隔。
-let foo = function(x) {
+let foo = function (x) {
     this.x = x;
 }
 let obj = {};
 foo.call(obj, 1);
 console.log(obj.x); // 1
+
+// 手写call
+
+Function.prototype._call = function (context) {
+    if (typeof this !== 'function') {
+        throw new TypeError('not function');
+    }
+    context = context || window;
+    context.fn = this;
+    const args = [...arguments].slice(1);
+    const result = context.fn(...args);
+    delete context.fn;
+    return result;
+}
